b00756d702a23a2c503ca23feac250444917b047
Tugaev Maxim <tugmaks@yandex.ru>
2015-02-16 15:48:09 +0400

    factory

---
+++ FlagIcon.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tugmaks\FlagIcons;
+use yii\helpers\Html;
+
+/**
+ * This is just an example.
+ */
+class FlagIcon extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
